14e3c356643fd769ee2287f1df7db03c61b93be7
ayrtonaoki <ayrtonsasouza@gmail.com>
2025-07-06 03:20:01 -0300

    Set Quiz and User relation

---
+++ app/models/quiz.rb
@@ -0,0 +1,4 @@
+class Quiz < ApplicationRecord
+  belongs_to :owner, class_name: 'User'
+  belongs_to :guest, class_name: 'User', optional: true
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :owned_quizzes, class_name: 'Quizz', foreign_key: 'owner_id'
+  has_many :guest_quizzes, class_name: 'Quizz', foreign_key: 'guest_id'
 end
