f90a95451b9611b39b6eff76dffcbfc4d6ab1222
ayrtonaoki <ayrtonsasouza@gmail.com>
2025-07-06 17:27:44 -0300

    Add User and Quiz relation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :owned_quizzes, class_name: 'Quizz', foreign_key: 'owner_id'
-  has_many :guest_quizzes, class_name: 'Quizz', foreign_key: 'guest_id'
+  has_many :owned_quizzes, class_name: 'Quiz', foreign_key: 'owner_id'
+  has_many :guest_quizzes, class_name: 'Quiz', foreign_key: 'guest_id'
 end
