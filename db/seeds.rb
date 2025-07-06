3527a07e48cc542e7c521e620aaf9e920e9f4e71
ayrtonaoki <ayrtonsasouza@gmail.com>
2025-07-06 03:20:09 -0300

    Add and install Faker gem

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,36 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+require 'faker'
+
+Quiz.delete_all
+User.delete_all
+
+users = []
+5.times do
+  users << User.create!(
+    name: Faker::Name.name,
+    email: Faker::Internet.unique.email,
+    password: "password",
+    password_confirmation: "password"
+  )
+end
+
+20.times do |i|
+  owner = users.sample
+  possible_guests = users.reject { |u| u == owner }
+  guest = [possible_guests.sample, nil].sample
+
+  Quiz.create!(
+    title: Faker::Lorem.sentence(word_count: 3),
+    description: Faker::Lorem.paragraph(sentence_count: 10),
+    owner: owner,
+    guest: guest
+  )
+end
+
+puts "Created #{users.count} users:"
+users.each { |u| puts "#{u.id} - #{u.name} (#{u.email})" }
+
+puts "\nCreated #{Quiz.count} quizzes:"
+Quiz.all.each do |quiz|
+  guest_name = quiz.guest ? quiz.guest.name : "No guest"
+  puts "Quiz #{quiz.id}: #{quiz.title}, Owner: #{quiz.owner.name}, Guest: #{guest_name}"
+end
