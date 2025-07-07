5001e329cf549735832fef34defe4ba925e7e183
ayrtonaoki <ayrtonsasouza@gmail.com>
2025-07-06 23:23:47 -0300

    Adds new validations to Answer attributes

--- db/migrate/20250706193317_create_answers.rb
+++ db/migrate/20250706193317_create_answers.rb
@@ -2,8 +2,8 @@ class CreateAnswers < ActiveRecord::Migration[7.2]
   def change
     create_table :answers do |t|
       t.references :question, null: false, foreign_key: true
-      t.string :text
-      t.boolean :correct_answer
+      t.string :text, null: false
+      t.boolean :correct_answer, null: false, default: false
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -28,8 +28,8 @@ ActiveRecord::Schema[7.2].define(version: 2025_07_06_193317) do
 
   create_table "answers", force: :cascade do |t|
     t.bigint "question_id", null: false
-    t.string "text"
-    t.boolean "correct_answer"
+    t.string "text", null: false
+    t.boolean "correct_answer", default: false, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["question_id"], name: "index_answers_on_question_id"
