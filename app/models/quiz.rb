007164f5bd3a702ddda6c91544e853c02cf7b701
ayrtonaoki <ayrtonsasouza@gmail.com>
2025-07-09 06:54:48 -0300

    Add validations to Quiz model

--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -1,6 +1,9 @@
 class Quiz < ApplicationRecord
   belongs_to :owner, class_name: 'User'
-  belongs_to :guest, class_name: 'User', optional: true
+  belongs_to :guest, class_name: 'User'
 
   has_many :questions
+
+  validates :title, presence: true
+  validates :description, presence: true
 end
