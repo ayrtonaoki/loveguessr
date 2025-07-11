fb78668fb2e620cb76438e5864aeb4bdb4bc263a
ayrtonaoki <ayrtonsasouza@gmail.com>
2025-07-11 00:08:06 -0300

    Remove unecessary code

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,8 +7,6 @@ RSpec.describe User, type: :model do
   end
 
   describe 'Validations' do
-    subject { build(:user) }
-
     context 'when has valid attributes' do
       it { should validate_presence_of(:email) }
       it { should validate_uniqueness_of(:email).case_insensitive }
