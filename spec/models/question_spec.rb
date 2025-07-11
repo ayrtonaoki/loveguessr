a136da82c1e5fba045ffd7df40516944e8fd76b0
ayrtonaoki <ayrtonsasouza@gmail.com>
2025-07-11 00:40:10 -0300

    Updates Question model test texts

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,50 +1,50 @@
 require 'rails_helper'
 
 RSpec.describe Question, type: :model do
-  describe 'Association' do
+  describe 'Associations' do
     it { should belong_to(:quiz) }
     it { should have_many(:answers) }
   end
 
   describe 'Validations' do
-    it { should validate_presence_of(:text) }
-    it { should validate_presence_of(:question_type) }
+    context 'when has valid attributes' do
+      it { should validate_presence_of(:text) }
+      it { should validate_presence_of(:question_type) }
 
-    it 'defines the correct enum mappings for question_type' do
-      expect(Question.question_types).to eq({
-        'multiple_choice' => 0,
-        'written_response' => 1
-      })
-    end
-
-    context 'has valid results' do
-      it 'when created with valid attributes' do
+      it 'creates a valid Question' do
         question = create(:question)
 
         expect(question).to be_valid
       end
+
+      it 'is a valid Question when has the correct enum mappings for question_type' do
+        expect(Question.question_types).to eq({
+          'multiple_choice' => 0,
+          'written_response' => 1
+        })
+      end
     end
 
-    context 'has invalid results' do
-      it 'when text is nil' do
+    context 'when has invalid attributes' do
+      it 'is invalid without a quizz' do
+        question = build(:question, quiz: nil)
+
+        expect(question).to be_invalid
+        expect(question.errors[:quiz]).to include("must exist")
+      end
+
+      it 'is invalid without a text' do
         question = build(:question, text: nil)
 
         expect(question).to be_invalid
         expect(question.errors[:text]).to include("can't be blank")
       end
 
-      it 'when question_type is nil' do
+      it 'is invalid without a question_type' do
         question = build(:question, question_type: nil)
 
         expect(question).to be_invalid
         expect(question.errors[:question_type]).to include("can't be blank")
-      end
-
-      it 'is invalid without a question' do
-        question = build(:question, quiz: nil)
-
-        expect(question).to be_invalid
-        expect(question.errors[:quiz]).to include("must exist")
       end
     end
   end
