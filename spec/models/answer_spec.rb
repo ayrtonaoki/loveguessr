ec6a6254248bc8868c2d6673f77e3bf2a4875a22
ayrtonaoki <ayrtonsasouza@gmail.com>
2025-07-11 00:39:55 -0300

    Updates Answer model test texts

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -1,40 +1,41 @@
 require 'rails_helper'
 
 RSpec.describe Answer, type: :model do
-  describe 'Association' do
+  describe 'Associations' do
     it { should belong_to(:question) }
   end
 
-  describe 'Validation' do
-    it { should validate_presence_of(:text) }
-    it { should allow_value(true, false).for(:correct_answer) }
+  describe 'Validations' do
+    context 'when has valid attributes' do
+      it { should validate_presence_of(:text) }
+      it { should allow_value(true, false).for(:correct_answer) }
 
-    context 'has valid results' do
-      it 'when created with valid attributes' do
+      it 'creates a valid Answer' do
         answer = create(:answer)
 
         expect(answer).to be_valid
       end
 
-      it 'has correct_answer as false by default when not set explicitly' do
+      it 'creates a valid Answer when correct_answer is false by default' do
         answer = Answer.new(text: 'Sample answer', question: create(:question))
+
         expect(answer.correct_answer).to eq(false)
       end
     end
 
-    context 'has invalid results' do
-      it 'when text is nil' do
-        answer = build(:answer, text: nil)
-
-        expect(answer).to be_invalid
-        expect(answer.errors[:text]).to include("can't be blank")
-      end
-
+    context 'when has invalid attributes' do
       it 'is invalid without a question' do
         answer = build(:answer, question: nil)
 
         expect(answer).to be_invalid
         expect(answer.errors[:question]).to include("must exist")
+      end
+
+      it 'is invalid without a text' do
+        answer = build(:answer, text: nil)
+
+        expect(answer).to be_invalid
+        expect(answer.errors[:text]).to include("can't be blank")
       end
 
       it 'is invalid when correct_answer is not true or false' do
