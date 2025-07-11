5fa6f3603907e3b754fe3cf70d58cf2ce82256d9
ayrtonaoki <ayrtonsasouza@gmail.com>
2025-07-11 00:40:16 -0300

    Updates Quiz model test texts

--- spec/models/quiz_spec.rb
+++ spec/models/quiz_spec.rb
@@ -1,33 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe Quiz, type: :model do
-  describe 'Association' do
+  describe 'Associations' do
     it { should belong_to(:owner) }
     it { should belong_to(:guest) }
     it { should have_many(:questions) }
   end
 
   describe 'Validations' do
-    it { should validate_presence_of(:title) }
-    it { should validate_presence_of(:description) }
+    context 'when has valid attributes' do
+      it { should validate_presence_of(:title) }
+      it { should validate_presence_of(:description) }
 
-    context 'has valid results' do
-      it 'when created with valid attributes' do
+      it 'creates a valid Quiz' do
         quiz = create(:quiz)
 
         expect(quiz).to be_valid
       end
     end
 
-    context 'has invalid results' do
-      it 'when title is nil' do
+    context 'when has invalid attributes' do
+      it 'is invalid without a title' do
         quiz = build(:quiz, title: nil)
 
         expect(quiz).to be_invalid
         expect(quiz.errors[:title]).to include("can't be blank")
       end
 
-      it 'when description is nil' do
+      it 'is invalid without a description' do
         quiz = build(:quiz, description: nil)
 
         expect(quiz).to be_invalid
